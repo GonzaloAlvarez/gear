76e59db0487ebb7c2bc0f2188308dc3c426db21c
Gonzalo Alvarez <gonzaloab@gmail.com>
2020-12-19 23:28:50 -0800

    Added arduino-cli

--- commands.sh
+++ commands.sh
@@ -1,4 +1,5 @@
 
 __COMMANDS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-alias terraform="${COMMANDS_PATH}/run terraform"
+alias terraform="${__COMMANDS_PATH}/run terraform"
+alias arduino-cli="${__COMMANDS_PATH}/run arduino-cli"
