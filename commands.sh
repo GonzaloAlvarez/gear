ea5c63feb0e15502a1805b204daab292232fba8b
Gonzalo Alvarez <gonzaloab@gmail.com>
2021-11-20 21:57:22 -0800

    Added gearconf to manage config files

--- commands.sh
+++ commands.sh
@@ -4,3 +4,5 @@ __COMMANDS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for i in $__COMMANDS_PATH/com/*; do
     alias $(basename $i)="${__COMMANDS_PATH}/run $(basename $i)"
 done
+
+alias gearconf="${__COMMANDS_PATH}/gearconf"
