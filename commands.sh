0c8f7f1f0cde093ebe993f4679c1e313309ff1b6
Gonzalo Alvarez <galvarez@axon.com>
2026-03-14 20:31:14 -0400

    added utils and chhostname script

--- commands.sh
+++ commands.sh
@@ -5,4 +5,8 @@ for i in $__COMMANDS_PATH/com/*; do
     alias $(basename $i)="${__COMMANDS_PATH}/run $(basename $i)"
 done
 
+for i in $__COMMANDS_PATH/util/*; do
+    alias $(basename $i)="${i}"
+done
+
 alias gearconf="${__COMMANDS_PATH}/gearconf"
